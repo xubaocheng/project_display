3ed25e5779441a9606b89b88dc7dd952de3026ec
Bates <xth5516739@163.com>
2020-03-12 10:32:53 +0800

    jixu

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,7 @@ VueRouter.prototype.push = function push(location, onResolve, onReject) {
 
 const whiteList = ['/login', '/home', '/404', '/401'] // 不重定向白名单
 const router = new VueRouter({
+    mode: 'history',
     routes: constantRouterMap
 })
 router.beforeEach((to, from, next) => {
